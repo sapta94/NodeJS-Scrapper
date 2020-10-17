3c1f092fc5f200e4672609c6d29c96b0c45c6388
sapta94 <dey7.kol@gmail.com>
2020-10-17 12:18:40 +0530

    added functions to parse links and store their count and params

--- index.js
+++ index.js
@@ -23,7 +23,39 @@ axios.get('https://medium.com').then(resp=>{
     parser.write(
         resp.data
     );
-    parser.end();    
+    parser.end();
+    parseLinksandStore();
+    
 }).catch(err=>{
     console.log('some error occured ',err)
 })
+
+function parseLinksandStore(){
+    linkObj={}
+    linkArr.forEach(element => {
+        let urlSplit = element.split('?')
+        let domainName = urlSplit[0]
+        if (domainName.slice(-1)=='/'){
+            domainName = domainName.substring(0,domainName.length-1)
+        }
+        if(linkObj[domainName]){
+            linkObj[domainName]['count']+=1
+            linkObj[domainName]['params'].concat(getParameterList(urlSplit[1]))
+        }else{
+            linkObj[domainName]={
+                count:1,
+                params:getParameterList(urlSplit[1])
+            }
+        }
+    });
+    console.log(linkObj)
+    return linkObj
+}
+
+function getParameterList(url){
+    let keyValues=url.split('&')
+    let keys = keyValues.map((item)=>{
+        return item.split('=')[0]
+    })
+    return keys
+}
