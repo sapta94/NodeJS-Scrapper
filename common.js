a3efab83735b10a717b658e0f6b49dc75c2c742e
sapta94 <dey7.kol@gmail.com>
2020-10-18 23:17:51 +0530

    moved functions to common file

---
+++ common.js
@@ -0,0 +1,34 @@
+function parseLinksandStore(linkArr){
+    linkObj={}
+    linkArr.forEach((element,index) => {
+        let urlSplit = element.split('?')
+        let domainName = urlSplit[0]
+        if (domainName.slice(-1)=='/'){
+            domainName = domainName.substring(0,domainName.length-1)
+        }
+        let paramList = (urlSplit[1])?getParameterList(urlSplit[1]):[]
+        if(linkObj[domainName]){
+            linkObj[domainName]['count']+=1
+            linkObj[domainName]['params'].concat(paramList)
+        }else{
+            linkObj[domainName]={
+                count:1,
+                params:paramList
+            }
+        }
+    });
+    return linkObj
+}
+
+function getParameterList(url){
+    let keyValues=url.split('&')
+    let keys = keyValues.map((item)=>{
+        return item.split('=')[0]
+    })
+    return keys
+}
+
+module.exports ={
+    getParameterList,
+    parseLinksandStore
+}
