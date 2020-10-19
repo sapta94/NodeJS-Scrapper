c897ec9578624ddf5cb9fa16c309342e6cc5e469
sapta94 <dey7.kol@gmail.com>
2020-10-19 23:12:09 +0530

    chnaging mongo url

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ const app = express();
 const bodyParser = require('body-parser')
 
 require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
-const link = require('./Models/link')
-
 
 app.use(bodyParser.json({limit: "10mb"}));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-mongoose.connect(`mongodb+srv://code-talks:${process.env.DB_PASSWORD}@cluster0.f3qmg.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,{
-  useNewUrlParser:true
+mongoose.connect('mongodb://mongo:27017/'+process.env.CONFIG_MONGODB_ADMINDB, {
+  useNewUrlParser: true,
+  user: process.env.CONFIG_MONGODB_ADMINUSERNAME,
+  pass: process.env.CONFIG_MONGODB_ADMINPASSWORD
 },(err)=>{
   if(err){
     console.log('Some error occurerd while Connecting to DB',err)
@@ -22,6 +21,15 @@ mongoose.connect(`mongodb+srv://code-talks:${process.env.DB_PASSWORD}@cluster0.f
   }
   console.log('Successfully Connected to DB!')
 })
+// mongoose.connect(`mongodb+srv://code-talks:${process.env.DB_PASSWORD}@cluster0.f3qmg.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,{
+//   useNewUrlParser:true
+// },(err)=>{
+//   if(err){
+//     console.log('Some error occurerd while Connecting to DB',err)
+//     return
+//   }
+//   console.log('Successfully Connected to DB!')
+// })
 
 const packages = {
     app,
