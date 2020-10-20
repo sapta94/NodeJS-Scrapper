9564befbdb8790bcbaf40d65a54a0bbc9368016a
sapta94 <dey7.kol@gmail.com>
2020-10-20 21:59:10 +0530

    added route for fetch api

--- routes/link-routes.js
+++ routes/link-routes.js
@@ -1,11 +1,14 @@
 const router = (packages) => {
     const apiRoutes = packages.express.Router();
     const {
-        linkCrawler
+        linkCrawler,findLinksDetails
       } = require('../controller/link-controller');
       
     apiRoutes.route('/start')
     .post(linkCrawler);
+
+    apiRoutes.route('/find')
+    .post(findLinksDetails);
 
     return apiRoutes
 }
