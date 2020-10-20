4d8f6c671493caa9a5bd0e6cc6cec06ea0322429
sapta94 <dey7.kol@gmail.com>
2020-10-20 21:59:35 +0530

    new api controller and recursion base ondition change

--- controller/link-controller.js
+++ controller/link-controller.js
@@ -14,6 +14,10 @@ module.exports.linkCrawler = async(req,res) => {
     }
 
     let data = await fetchLinks(url,{},[],res)
+    return res.json({
+        data,
+        status:true
+    })
 }
 
 //globall arr that helps to track all the links visited till now
@@ -26,32 +30,35 @@ let visitedArr=[]
 */
 function fetchLinks(url,linkDetails,linkArr,res){
     console.log('called for ',url)
-    //base condition for recursion to break
-    if(linkArr.length==0 && Object.keys(linkDetails).length!=0){
-        console.log('length is done and dusted')
-        return res.json({
-            data,
-            status:true
-        })
-    }
+    
     //promise to synchronize multiple url calls
     return new Promise((resolve,reject)=>{
+
+        //base condition for recursion to break
+        if(linkArr.length==0 && Object.keys(linkDetails).length!=0){
+            console.log('length is done and dusted')
+            return resolve(linkDetails)
+        }
 
         //parser that parses the html response and returns the <a> tags
         const parser = new htmlparser2.Parser({
             onopentag(name, attribs) {
                 if (name === "a") {
                     //if <a> tag encountered then check if it is visited or not and is of medium.com domain
-                    if (attribs.href.includes('medium.com/') && !visitedArr.includes(attribs.href)){
+                    let domainName = attribs.href.split('?')[0]
+                    if (domainName.slice(-1)=='/'){
+                        domainName = domainName.substring(0,domainName.length-1)
+                    }
+                    if (attribs.href.includes('medium.com/') && !visitedArr.includes(domainName)){
                         linkArr.push(attribs.href)
-                        visitedArr.push(attribs.href)
+                        visitedArr.push(domainName)
                     }
                     
                 }
             },
             onclosetag(tagname) {
                 if (tagname === "a") {
-                    //console.log("That's it?!");
+                    //console.log('Link parsed')
                 }
             },
         });
@@ -78,7 +85,19 @@ function fetchLinks(url,linkDetails,linkArr,res){
                 }
                 let linkModel = new link(eachLink)
                 try{
-                    await linkModel.save()
+                    let exists = await link.findOne({linkName:item})
+                    //check if link already exists , then update count else insert 
+                    if(exists && exists['count']>0){
+                        //console.log(exists)
+                        let newParams = exists.params.concat(eachLink.params)
+                        let newCount = parseInt(exists['count'])+1
+                        console.log('newCount is ',newCount)
+                        await link.updateOne({linkName:item},{$set:{linkName:item,count:newCount,params:newParams}})
+                    }
+                    else{
+                        await linkModel.save()
+                    }
+                    
                 }catch(err){
                     console.log('mongo err', err)
                 }
@@ -92,11 +111,40 @@ function fetchLinks(url,linkDetails,linkArr,res){
                 await fetchLinks(toCrawlLinks[item],linkDetails,linkArr.slice(5,linkArr.length),res)
             }
             
-        }).catch(err=>{
+        }).catch(async err=>{
             console.log('some error occured ')
             console.log('Links --> ',linkArr.length)
-            return resolve(linkDetails)
+            
+            //deque first 5 links from linkArr
+            let toCrawlLinks = linkArr.slice(0,5)
+            for (item in toCrawlLinks){
+                //recursively call the 5 dequed urls 
+                await fetchLinks(toCrawlLinks[item],linkDetails,linkArr.slice(5,linkArr.length),res)
+            }
         })
     })
     
+}
+
+
+module.exports.findLinksDetails=async (req,res)=>{
+    let url = req.body.url||null;
+    let data=[]
+    try{
+        if(url){
+            data = await link.find({linkName:url})
+        }else{
+            data = await link.find()
+        }
+        return res.json({
+            data,
+            status:true
+        })
+    }catch(err){
+        console.log('some error occured ',err)
+        return res.status(500).json({
+            status:false,
+            data
+        })
+    }
 }
